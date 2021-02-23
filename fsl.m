9fff74830bf3e13f0de3153b0b3b5bb7d4bf6645
Christophe Phillips <c.phillips@uliege.be>
2021-02-23 18:56:00 +0100

    Major update: * explicit writing of command calls with 'sprintf' for easier check * change the way output files are selected and moved -> use regexp filtering based on list * check any issue with output status

--- fsl.m
+++ fsl.m
@@ -16,43 +16,61 @@
 %   required input files into that directory.
 
 function [status, cmd_out] = fsl(cmd, in_files, out_patterns)
-    % Create temporary directory
-    cwd = pwd;
-    twd = tempname(tempdir);
-    err = mkdir(twd);
-    if err ~= 1
-        status = 0;
-        cmd_out = 'Unable to create temporary directory';
-        return
-    end
-    % Copy all the input files
-    for ii = 1 : size(in_files,1)
-        copyfile(in_files(ii,:), twd);
-    end
-    % Move to temporary directory
-    cd(twd);
-    % Run the FSL command
-    docker_exec = getenv('DOCKER_EXEC');
-    fsl_img = getenv('FSL_IMG');
-    docker_cmd = sprintf('%s run --rm -v %s:/home/fsl %s', ...
-        docker_exec, twd, fsl_img);
-%     docker_cmd = join([docker_exec, 'run', '--rm', '-v', strcat(twd, ':', '/home/fsl'), fsl_img]);
-
-    [status, cmd_out] = system( sprintf('%s %s', docker_cmd, cmd) );
-    % Removing the '-echo' which was generating an error
-    
-%     [status, cmd_out] = system(join([docker_cmd, cmd]), '-echo');
-
-% Copy all the output files to the initial directory
-    for ii = 1 :size(out_patterns,1)
-        movefile(out_patterns(ii), cwd);
-    end
-    % Clean temporary directory and go back to initial directory
-    delete('*');
-    cd(cwd);
-    rmdir(twd);
-    if ~exist(twd,'dir')
-        fprintf('\nTemporary folder removed');
-    end
-%     isfolder(twd)
+% Create temporary directory
+cwd = pwd;
+twd = tempname(tempdir);
+err = mkdir(twd);
+if err ~= 1
+    status = 0;
+    cmd_out = 'Unable to create temporary directory';
+    return
 end
+% Copy all the input files
+for ii = 1 : size(in_files,1)
+    copyfile(in_files(ii,:), twd);
+end
+% Move to temporary directory
+cd(twd);
+% Run the FSL command
+docker_exec = getenv('DOCKER_EXEC');
+fsl_img = getenv('FSL_IMG');
+docker_cmd = sprintf('%s run --rm -v %s:/home/fsl %s', ...
+    docker_exec, twd, fsl_img);
+
+% Removing the '-echo' which was generating an error
+full_cmd = sprintf('%s %s', docker_cmd, cmd);
+[status, cmd_out] = system( full_cmd );
+
+if status % something went wrong
+    fprintf('\nPROBLEM: this command did not work: \n%s\n',full_cmd)
+    fprintf('\nOutput message states: \n%s\n',cmd_out);
+end
+% Copy all the output files to the initial directory, based on regexp
+fn_list = ls; % list of current files
+for ii = 1:size(out_patterns,1)
+    fn_mv = file_filter(fn_list,out_patterns(ii,:));
+    for jj = 1:size(fn_mv,1)
+        movefile(deblank(fn_mv(jj,:)), cwd);
+    end
+end
+% Clean temporary directory and go back to initial directory
+delete('*');
+cd(cwd);
+rmdir(twd);
+if ~exist(twd,'dir')
+    fprintf('\nTemporary folder removed\n');
+end
+
+end
+
+function fn_mv = file_filter(fn_list,fn_regexp)
+% Find subset of filenames based on list and reg-expression
+if ~iscell(fn_list)
+    fn_list = cellstr(fn_list);
+end
+% get list of matching filenames
+ll_list = ~cellfun('isempty',regexp(fn_list,fn_regexp));
+fn_mv = char(fn_list(ll_list));
+
+end
+
