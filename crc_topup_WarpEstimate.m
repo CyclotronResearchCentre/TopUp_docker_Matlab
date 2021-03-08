c60f2e327d5af18ca34094a8468a26ea0f21849b
Christophe Phillips <c.phillips@uliege.be>
2021-03-08 23:07:48 +0100

    Return filename as output
    Filenames of
    - warps spline coefficients
    - filed in Hertz

--- crc_topup_WarpEstimate.m
+++ crc_topup_WarpEstimate.m
@@ -1,4 +1,4 @@
-function [a, b] = crc_topup_WarpEstimate(fnD1,fnD2,fnAcqpar,fnConfig,dOut)
+function [fn_TUsc, fn_TUhz] = crc_topup_WarpEstimate(fnD1,fnD2,fnAcqpar,fnConfig,dOut)
 % High-level function to estimate the topup warps from 2 sets of image.
 % 
 % INPUT
@@ -9,7 +9,8 @@ function [a, b] = crc_topup_WarpEstimate(fnD1,fnD2,fnAcqpar,fnConfig,dOut)
 % dOut      : output folder [optional]
 % 
 % OUTPUT
-% 
+% fn_TUsc   : filename of spline coeficients (.nii.gz image)
+% fn_TUhz   : filename of field in Hertz (.nii.gz image)
 % 
 % NOTES
 % 1/ The results and intermediary files are placed in the 'dOUT' folder. If
@@ -26,12 +27,17 @@ function [a, b] = crc_topup_WarpEstimate(fnD1,fnD2,fnAcqpar,fnConfig,dOut)
 % Written by C. Phillips, 2021.
 % GIGA Institute, University of Liege, Belgium
 
+%% No check on input for the moment
+
+%% Parameters
+pref_sc = 'TUsc_'; % estimated spline coeficients
+pref_hf = 'TUhf_'; % estimated field in Hertz
+
 %% Setup, Docker attributes
+% -> should it be defined somewhere else or if at all?
 setenv('DOCKER_EXEC', 'docker');            % The command to run docker.
 setenv('FSL_IMG', 'topup:6.0.3-20210212');  % The name of the topup image.
 
-
-%% No check on input for the moment
 
 %% Dealing with the output folder
 if nargin<5 || isempty(dOut)
@@ -42,20 +48,22 @@ if ~exist(dOut,'dir'), mkdir(dOut); end
 
 %% Combine the 2 sets of 3D images into a 4D image
 fn_fmapfunc4D_topup = spm_file(fnD1(1,:),'suffix','_4topup');
-V4fmapfunc4D_topup = spm_file_merge(char(fnD1,fnD2),fn_fmapfunc4D_topup);
-
+V4fmapfunc4D_topup = spm_file_merge(char(fnD1,fnD2),fn_fmapfunc4D_topup); %#ok<*NASGU>
 
 %% Estimate the warps
-% 4D volume with AP/PA data (2 vols each)
-% fn_data = 'C:\3_Code\topup_docker_data\TestData\fmap\sub-s011_ses-baseline_dir-PA_epi_4topup.nii';
-% Parameter and config file
-% fn_acqParam = 'C:\3_Code\topup_docker_data\TestData\acqparams.txt';
-% fn_cnf      = 'C:\3_Code\topup_docker_data\TestData\b02b0.cnf';
 
-% Call, with prefix 'TUsc_' & 'TUhf_' for the 2 output (spline coefs and 
-% Hertz field)
+% Call to create the 2 output.
 [status, cmd_out] = crc_topup_estimate( ...
-    fn_fmapfunc4D_topup, fnAcqpar, fnConfig, char('TUsc_','TUhf_') );
+    fn_fmapfunc4D_topup, fnAcqpar, fnConfig, char(pref_hf,pref_hf) );
+if status
+    err_msg = sprintf(['\nThere was a problem.', ...
+        '\n\tHere is the error message collected:', ...
+        '\n\t%s\n'],cmd_out);
+    error('DockerTU:WarpEstimate',err_msg); %#ok<*SPERR>
+end
 
+fn_TUsc = spm_file(fn_fmapfunc4D_topup, ...
+    'prefix',pref_sc, 'suffix','_fieldcoef', 'ext','.nii.gz');
+fn_TUhz = spm_file(fn_fmapfunc4D_topup, 'prefix',pref_hf, 'ext','.nii.gz');
 
 end
