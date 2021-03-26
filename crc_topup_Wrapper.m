2559b403a9a7fc6edd04a4e397c26b150cb93c4e
Christophe Phillips <c.phillips@uliege.be>
2021-03-26 20:02:47 +0100

    Basic wrapper doing the job
    It is not crashing but results must be further checked
    Need
    + more flexibility and options
    + matlabbatch interfacing

--- crc_topup_Wrapper.m
+++ crc_topup_Wrapper.m
@@ -1,9 +1,9 @@
-function crc_topup_wrapper(fn_func,fn_fmap)
+function fn_urfunc = crc_topup_Wrapper(fn_func,fn_fmap, fn_Acqpar, fn_Config)
 % Function to automatize the processing by taking as input the set of
 % many functional images to unwarp & realign, plus a few "fieldmap" images 
 % with the phase-encdoding in the opposite direction.
-
-% to wrap arround the other 2 high-level functions:
+% 
+% This wraps arround the other 2 high-level functions:
 % - crc_topup_WarpEstimate : estimate the warps from 2 sets of images
 % - crc_topup_WarpApply    : apply the warps on one set of images
 % 
@@ -14,12 +14,21 @@ function crc_topup_wrapper(fn_func,fn_fmap)
 % 2. realign, "estimate & write", the whole set of functional images
 % 3. apply the estimated warps onto the realigned images
 % 
-% 
 % INPUT
+% fn_func     : char array of filenames of the functional images to correct
+% fn_fmap     : char array of filenames of the fieldmap images, with the 
+%               PE direction in opposite direction of the functional data
+% fn_Acqpar   : filename of the acquisition parameter
+% fn_Config   : filename of the config file for TopUp
 % 
 % OUTPUT
+% fn_urfunc   : char array of filenames of the functional images after
+%               realignement and topup unwarping
 % 
 % NOTES
+% So far the code considers that 2 func and 2 fmap images should be used
+% for the TU estimates, which MUST correspond to the acquisition parameters
+% in the fn_Acqpar file!
 %__________________________________________________________________________
 % Copyright (C) 2021 Cyclotron Research Centre
 
@@ -27,14 +36,21 @@ function crc_topup_wrapper(fn_func,fn_fmap)
 % GIGA Institute, University of Liege, Belgium
 
 %% Estimate the warps from a subset of func & fmap images 
-% Example 2 images with PE straight and reverse
-[fn_TUsc, fn_TUhz] = ...
-    crc_topup_WarpEstimate(fn_D1, fn_D2, fn_Acqpar, fn_Config, dOut); %#ok<*ASGLU>
+% Pick 2 func and fmap images 
+fn_D1 = fn_func(1:2,:);
+fn_D2 = fn_fmap(1:2,:);
+
+fn_TUsc = crc_topup_WarpEstimate(fn_D1, fn_D2, fn_Acqpar, fn_Config);
 
 %% Realign and resample the functional data
+% for the moment, using all the default parameters
+spm_realign(fn_func);
+spm_reslice(fn_func)
+% get the name of realigned and resliced functional images
+fn_func_rr = spm_file(fn_func,'prefix',spm_get_defaults('realign.write.prefix'));
 
-%% Apply the warps
+%% Apply the warps on r&r functional images
 % Call to mid-level function to apply the unwarping
-fn_uwD = crc_topup_WarpApply(fn_D, fn_Acqpar, fn_TUsc);
+fn_urfunc = crc_topup_WarpApply(fn_func_rr, fn_Acqpar, fn_TUsc);
 
 end
