5b747cdca9d035a6b50eac0707be0d7878753c56
Christophe Phillips <c.phillips@uliege.be>
2021-03-31 12:45:18 +0200

    Adding feature: returning the suffix itself

--- crc_rm_suffix.m
+++ crc_rm_suffix.m
@@ -1,9 +1,10 @@
-function fn_out = crc_rm_suffix(fn_in, regexp_filt)
-% Remove trailing suffix to a filename based on a regexp expression.
+function [fn_out,sf_out] = crc_rm_suffix(fn_in, regexp_filt)
+% Remove trailing suffix to a (list of) filename(s) based on a regular 
+% expression and returning the picked (list of) suffix(es).
 % For example, to remove the '_01234' suffix from 3D image files obtaining 
-% after splitting a 4D image file with spm_split, should use '_\d{5,5}$'
-% as this picks the index of '_' followed by 5 digits at the end of the
-% filename.
+% after splitting a 4D image file with spm_split, one should use 
+% '_\d{5,5}$' as this picks the index of '_' followed by 5 digits at the 
+% end of the filename. The suffix returned would thus be '_01234'.
 % 
 % Note that 
 % - the function does NOT check the validity of the regexp passed
@@ -12,11 +13,12 @@ function fn_out = crc_rm_suffix(fn_in, regexp_filt)
 % 
 % INPUT
 % fn_in         : (char/cell array of) filename(s) to consider
-% regexp_filt   : recular expression to match, default '_\d{5,5}$'
+% regexp_filt   : regular expression to match, default '_\d{5,5}$'
 % 
 % OUTPUT
 % fn_out        : (char/cell array of) filename(s) after removing the
 %                  suffix, if found.
+% sf_out        : (char/cell array of) suffixes removed, if found
 %__________________________________________________________________________
 % Copyright (C) 2021 Cyclotron Research Centre
 
@@ -44,6 +46,7 @@ end
 % Find out number of files to deal with and prepare cell output
 Nfn = numel(fn_inC);
 fn_outC = cell(size(fn_inC));
+sf_outC = cell(size(fn_inC));
 
 % Loop over all files and do the job
 for ifn = 1:Nfn
@@ -56,8 +59,10 @@ for ifn = 1:Nfn
     % remove it if possible
     if ~isempty(tt)
         fn_nam_cl = fn_nam(1:tt-1);
+        sf_outC{ifn} = fn_nam(tt:end);
     else
         fn_nam_cl = fn_nam;
+        sf_outC{ifn} = [];
     end
 
     % rebuild output, without the matched part if possible
@@ -71,8 +76,10 @@ end
 % Deal with out, i.e. return same type of array
 if ~flag_cell
     fn_out = char(fn_outC);
+    sf_out = char(sf_outC);
 else
     fn_out = fn_outC;
+    sf_out = sf_outC;
 end
 
 
