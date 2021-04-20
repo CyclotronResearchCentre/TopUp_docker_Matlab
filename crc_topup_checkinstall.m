6550e0ffa4875a0fe1a46ba9d732096913d23790
Christophe Phillips <c.phillips@uliege.be>
2021-04-20 17:10:54 +0200

    Short update: - use the new default file - improve returned error messages

--- crc_topup_checkinstall.m
+++ crc_topup_checkinstall.m
@@ -12,27 +12,41 @@ function [flag, cmd_out] = crc_topup_checkinstall
 % Written by C. Phillips, 2021.
 % GIGA Institute, University of Liege, Belgium
 
+% Get defaults
+Dimg_name = crc_topup_get_defaults('Dimg_name');
+Dimg_vers = crc_topup_get_defaults('Dimg_vers');
+
 % Calling docker to see which image is available
 [status, cmd_out] = system('docker images');
 % if status is 1, then there is an issue
 
 flag = ~status;
 if status
-    fprintf('\nThere was a problem.');
+    fprintf('\nThere was a problem with Docker.');
+    fprintf('\n\tCheck that Docker is installer and running.');
     fprintf('\n\tHere is the error message collected:');
     fprintf('\n\t%s',cmd_out);
     fprintf('\n');
 else
-    S = regexp(cmd_out,'topup','once');
-    if isempty(S)
+    S1 = regexp(cmd_out,Dimg_name,'once'); % check name
+    S2 = regexp(cmd_out,Dimg_vers,'once'); % check version
+    if isempty(S1)
         fprintf('\nThere was a problem.');
-        fprintf('\n\tThe "topup" image is not available.');
-        fprintf('\n\tHere is the error message collected:');
+        fprintf('\n\tThe "%s" image is not available.',Dimg_name);
+        fprintf('\n\tHere are the Docker images collected:');
+        fprintf('\n%s',cmd_out);
+        fprintf('\n');        
+        flag = false;
+    elseif isempty(S2)
+        fprintf('\nThere was a problem.');
+        fprintf('\n\tThe "%s" image is available but not with the expected Tag (%s).', ...
+            Dimg_name, Dimg_vers);
+        fprintf('\n\tHere are the Docker images collected:');
         fprintf('\n%s',cmd_out);
         fprintf('\n');        
         flag = false;
     else
-        fprintf('\nInstallation of "topup" docker seems fine:');
+        fprintf('\nInstallation of "%s" docker seems fine:',Dimg_name);
         fprintf('\n%s',cmd_out);
         fprintf('\n');        
     end
