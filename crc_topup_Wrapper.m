7213b6a4c375bfcb2be4cc2ed87665b74ae0ef6f
Christophe Phillips <c.phillips@uliege.be>
2021-04-22 17:58:08 +0200

    Now handling mulitple fMRI sessions in one go: - adding loop for per session estimate & application of TU correction - combining all sessions for realign, estimate & write, with single mean image creation

--- crc_topup_Wrapper.m
+++ crc_topup_Wrapper.m
@@ -18,22 +18,35 @@ function [fn_urfunc, fn_umean] = crc_topup_Wrapper(fn_func,fn_fmap, fn_Acqpar, f
 %   functional and fieldmaps.
 % 2. realign, "estimate & write", the whole set of functional images
 % 3. apply the estimated warps onto the realigned images
+% 
+% For multiple fMRI sessions, each session must include both an fMRI series
+% and "fieldmap" images (with opposite PE direction). The processing will
+% be the same as for a single session, except for the SPM realignment, with
+% "estimate & write", where all essions are realigned together generating a
+% single mean image. The acquisition parameter and config files are assumed
+% to be the same for all the sessions.
 %
 % INPUT
-% fn_func     : char array of filenames of the functional images to correct
-% fn_fmap     : char array of filenames of the fieldmap images, with the
-%               PE direction in opposite direction of the functional data
-% fn_Acqpar   : filename of the acquisition parameter
-% fn_Config   : filename of the config file for TopUp
+% fn_func   : char array of filenames of the functional images to correct,
+%             or cell array of these for multiple sessions
+% fn_fmap   : char array of filenames of the fieldmap images, with the
+%             PE direction in opposite direction of the functional data,
+%             or cell array of these for multiple sessions
+% fn_Acqpar : filename of the acquisition parameter
+% fn_Config : filename of the config file for TopUp
 %
 % OUTPUT
-% fn_urfunc   : char array of filenames of the functional images after
-%               realignement and topup unwarping
+% fn_urfunc : char array of filenames of the functional images after
+%             realignement and topup unwarping, or cell array of these for 
+%             multiple sessions
+% fn_umean  : filename of the unwarped mean functional image, if it was
+%             created during the realignment.
 %
 % NOTES
-% So far the code considers that 2 func and 2 fmap images should be used
-% for the TU estimates, which MUST correspond to the acquisition parameters
-% in the fn_Acqpar file!
+% So far the code considers that 2 func and 2 fmap images (2 is a default 
+% parameter) should be used for the TU estimates, which MUST correspond to 
+% the acquisition parameters in the acquisition parameter (fn_Acqpar) file!
+% 
 %__________________________________________________________________________
 % Copyright (C) 2021 Cyclotron Research Centre
 
@@ -50,11 +63,38 @@ if ~fl_param
 end
 
 %% Estimate the warps from a subset of func & fmap images
-% Pick 2 func and fmap images
-fn_D1 = fn_func(1:N_fn,:);
-fn_D2 = fn_fmap(1:N_fn,:);
 
-fn_TUsc = crc_topup_WarpEstimate(fn_D1, fn_D2, fn_Acqpar, fn_Config);
+% Deal with single vs. multiple sessions
+if iscell(fn_func) && iscell(fn_fmap)
+    N_sess = numel(fn_func);
+    if N_sess ~= numel(fn_fmap)
+        err_msg = sprintf(['\nInput mismatch for data.', ...
+        '\n\tThere are %d "func" vs %d "fmap" sets!\n'], ...
+        N_sess, numel(fn_fmap));
+        error('DockerTU:fMRIWrapper',err_msg) %#ok<*SPERR>
+    end
+    fn_func_c = fn_func;
+    fn_fmap_c = fn_fmap;
+    fl_char = false;
+elseif ischar(fn_func) && ischar(fn_fmap)
+    N_sess = 1;
+    fn_func_c{1} = fn_func;
+    fn_fmap_c{1} = fn_fmap;
+    fl_char = true;
+else
+    error('DockerTU:fMRIWrapper','\nInput mismatch for data.\n')
+end
+
+% Loop through the sessions
+fn_TUsc = cell(N_sess,1);
+for i_sess = 1:N_sess
+    % Pick 2 func and fmap images
+    fn_D1 = fn_func_c{i_sess}(1:N_fn,:);
+    fn_D2 = fn_fmap_c{i_sess}(1:N_fn,:);
+
+    fn_TUsc{i_sess} = crc_topup_WarpEstimate(fn_D1, fn_D2, ...
+        fn_Acqpar, fn_Config);
+end
 
 %% Realign and resample the functional data
 % for the moment, using all the default parameters except for prefix, set
@@ -62,17 +102,41 @@ fn_TUsc = crc_topup_WarpEstimate(fn_D1, fn_D2, fn_Acqpar, fn_Config);
 rr_prefix = crc_topup_get_defaults('rr_prefix');
 
 % Estimate realignement and resample
-spm_realign(fn_func);
-spm_reslice(fn_func,struct('prefix',rr_prefix)) 
+spm_realign(fn_func_c);
+spm_reslice(fn_func_c,struct('prefix',rr_prefix)) 
 % get the name of realigned and resliced functional images
-fn_func_rr = spm_file(fn_func,'prefix',rr_prefix);
-fn_func_mean = spm_file(fn_func(1,:),'prefix','mean');
+fn_func_c_rr = cell(N_sess,1);
+for i_sess = 1:N_sess
+    fn_func_c_rr{i_sess} = spm_file(fn_func_c{i_sess},'prefix',rr_prefix);
+end
+fn_func_mean = spm_file(fn_func_c{1}(1,:),'prefix','mean');
+if ~exist(fn_func_mean,'file')
+    fn_func_mean = '';
+    % in case it was not created...
+end
 
 %% Apply the warps on r&r functional images
 % Call to mid-level function to apply the unwarping,
-% indicating that last volume is the mean
-fn_D = char(fn_func_rr , fn_func_mean); % functional + mean
-[fn_urfunc, fn_umean] = crc_topup_WarpApply(fn_D, fn_Acqpar, fn_TUsc, 1);
+fn_urfunc_c = cell(N_sess,1);
+for i_sess = 1:N_sess
+    if i_sess==1 && ~isempty(fn_func_mean)
+        % deal with 1st session + mean, if possible
+        fn_D = char( fn_func_c_rr{i_sess} , fn_func_mean); %#ok<*USENS> 
+        [fn_urfunc_c{i_sess}, fn_umean] = ...
+            crc_topup_WarpApply(fn_D, fn_Acqpar, fn_TUsc{i_sess}, 1);
+    else
+        fn_urfunc_c{i_sess} = ...
+            crc_topup_WarpApply( ...
+                fn_func_c_rr{i_sess}, fn_Acqpar, fn_TUsc{i_sess}, 0);
+    end
+end
+
+if fl_char
+    % return char arrays if input was a char array, i.e. single session
+    fn_urfunc = char(fn_urfunc_c);
+else
+    fn_urfunc = fn_urfunc_c;
+end
 
 end
 
